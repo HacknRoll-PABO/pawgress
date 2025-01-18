8031981ea265015e60514ed1b1919a87b5bb19db
oM <omtirodkar03@gmail.com>
2025-01-18 16:38:44 +0800

    Completed add, delete and update

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, request
-import os
+from datetime import date
 from openai import OpenAI
+import os
+import json
 
 app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+today = date.today()
+db = SQL("sqlite:///task_list.db")
 
 client = OpenAI(
     api_key=os.environ.get("OPENAI_API_KEY"),  # This is the default and can be omitted
@@ -30,16 +37,36 @@ def onboarding():
 
 @app.route("/addtask", methods=["POST"])
 def add_task():
+    task = json.loads(request.data)
+    task_id = task.get("id")
+    task_name = task.get("content")
+    task_priority = task.get("priority")
+    task_completion = task.get("completed")
+    task_date = today.strftime("%d/%m/%Y")
+
+    db.execute("INSERT INTO task_list (task_id, task_name, task_desc, task_priority, task_date) VALUES(?, ?, ?, ?, ?);", task_id, task_name, task_priority, task_completion, task_date)
     return "Hello"
 
 
 @app.route("/removetask", methods=["POST"])
 def remove_task():
+    task = json.loads(request.data)
+    task_id = task.get("id")
+
+    user_data = db.execute("DELETE * from task_list WHERE task_id = ?;", task_id)
     return "Hello"
 
 
 @app.route("/updatetask", methods=["POST"])
 def update_task():
+    task = json.loads(request.data)
+    task_id = task.get("id")
+    task_name = task.get("content")
+    task_priority = task.get("priority")
+    task_completion = task.get("completed")
+    task_date = today.strftime("%d/%m/%Y")
+
+    db.execute("UPDATE task_list SET (task_name, task_desc, task_priority, task_date) VALUES(?, ?, ?, ?) WHERE id = ?;", task_name, task_priority, task_completion, task_date, task_id)
     return "Hello"
 
 
